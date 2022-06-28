5a8e8eaa9690c25415b042f110acd0b1b5cd499d
Makkapakka <1961605387@qq.com>
2022-06-28 17:02:38 +0800

    配置全局跨域

---
+++ src/main/java/cn/lycan/simpleblogjava/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package cn.lycan.simpleblogjava.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Makkapakka
+ * @date 2022-6-28
+ * @package_name cn.lycan.simpleblogjava.config
+ * @description 后端配置全局跨域处理
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
