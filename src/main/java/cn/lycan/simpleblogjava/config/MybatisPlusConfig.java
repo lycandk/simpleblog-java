aa8b3d916c2eaa4c101b1a34312b1b6cc79678e2
Makkapakka <1961605387@qq.com>
2022-06-29 15:42:26 +0800

    更正分页插件（新版）

--- src/main/java/cn/lycan/simpleblogjava/config/MybatisPlusConfig.java
+++ src/main/java/cn/lycan/simpleblogjava/config/MybatisPlusConfig.java
@@ -1,10 +1,14 @@
 package cn.lycan.simpleblogjava.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Collections;
 
 /**
  * @author Makkapakka
@@ -19,6 +23,19 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInnerInterceptor paginationInnerInterceptor() {
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInnerInterceptor.setMaxLimit(5L);
+        paginationInnerInterceptor.setDbType(DbType.MYSQL);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInnerInterceptor.setOptimizeJoin(true);
         return paginationInnerInterceptor;
     }
+    
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.setInterceptors(Collections.singletonList(paginationInnerInterceptor()));
+        return mybatisPlusInterceptor;
+    }
+    
 }
